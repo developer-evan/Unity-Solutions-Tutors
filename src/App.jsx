2bb07a227b57551f669870b34c2ff157eb6fd70d
Evans-254 <71435810+Evans-254@users.noreply.github.com>
2023-10-07 23:03:24 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable no-unused-vars */
 // import { useState } from "react";
-import Login from "./Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import MainLayout from "./layout/MainLayout";
+// import Login from "./Login";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import MainLayout from "./layout/MainLayout";
 import AppRouter from "./routes/AppRouter";
 // import MainLayout from "./MainLayout";
 
